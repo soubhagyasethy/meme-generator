cee90fab34c6715559230951035e141c287f7f4c
soubhagyasethy <ssethy017@gmail.com>
2023-01-28 23:59:12 +0530

    feat: added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Header from "./components/header"
 
 function App() {
   return (
     <div className="App">
-      <h1>Meme Generator</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return(
+        <header className="header">
+            <img className="header-image" src="../images/Troll face.svg" alt="laughing face"/>
+            <h2 className="header-text">Meme Genertor</h2>
+            <h3 className="header-subtext">share it for fun</h3>
+        </header>
+    )
+}
