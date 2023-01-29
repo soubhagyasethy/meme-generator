bce5bac0649d7c2ac089acfea3eaa4e8877fb5af
soubhagyasethy <ssethy017@gmail.com>
2023-01-29 23:25:21 +0530

    refactor: updated header text

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ export default function Header() {
     return(
         <header className="header">
             <img className="header-image" src="../images/Troll face.svg" alt="laughing face"/>
-            <h2 className="header-text">Meme Genertor</h2>
+            <h2 className="header-text">Meme Generator</h2>
             <h3 className="header-subtext">share it for fun</h3>
         </header>
     )
