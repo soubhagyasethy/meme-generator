6272a3054e3b4f040a7ce56f78be7a7022eb6091
soubhagyasethy <ssethy017@gmail.com>
2023-01-29 14:25:17 +0530

    style: updated meme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/header"
+import Meme from "./components/meme"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
---
+++ src/components/meme.js
@@ -0,0 +1,11 @@
+
+export default function Meme() {
+    return(
+        <div className="meme">
+            <input className="input" />
+            <input className="input" />
+            <button className="button">Get a new meme image</button>
+            
+        </div>
+    )
+}
