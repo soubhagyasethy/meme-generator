c8fdca529830e7e0625882bcca4f910bdd31410c
soubhagyasethy <ssethy017@gmail.com>
2023-01-30 19:32:36 +0530

    updated outpt image

--- src/components/meme.js
+++ src/components/meme.js
@@ -1,12 +1,29 @@
+import {useState} from "react";
+import memeData from "../memesData"
+
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = useState("");
+
+    function clickHandler() {
+        const memesArray = memeData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const randomUrl = memesArray[randomNumber].url;
+        console.log(randomUrl);
+        setMemeImage(randomUrl);
+
+    }
     return(
         <main className="main">
             <div className="meme">
             <input className="input" type="text"  placeholder="top text"/>
+
             <input className="input" type="text" placeholder="bottom text"/>
-            <button className="button">Get a new meme image</button>
+
+            <button className="button" onClick={clickHandler}>Get a new meme image</button>
             </div>
+
+            <img className="meme-image" src={memeImage} alt="meme" />
         </main>
     )
 }
